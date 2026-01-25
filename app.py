00d3b6d30944c283fe068f7b64416311f12472f2
corentinpilar <corentin.pilar@gmail.com>
2026-01-25 20:00:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -139,4 +139,4 @@ with tab2:
                 st.error(f"Erreur : {e}")
 
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #555; font-size: 0.85em;'>© LE FAUX SOIR - PRODUCTION</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #555; font-size: 0.85em;'>© Copyright - Corentin Pilarczyk</div>", unsafe_allow_html=True)
